1ef905d1bd968c6abdb7ed176f38c5348c8d4e21
Chai <k.chaig1@gmail.com>
2022-03-03 19:32:00 +0100

    Added SDL2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 
 using namespace std;
 
 int main()
 {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
     cout << "Hello World!" << endl;
+    SDL_Quit();
     return 0;
 }
