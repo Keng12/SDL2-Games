6b7a9f6a4239443a1c9f40578fbceb349fc4328d
Chai <k.chaig1@gmail.com>
2022-03-04 20:20:43 +0100

    Added rules

--- src/gol_util.hpp
+++ src/gol_util.hpp
@@ -8,27 +8,39 @@
 
 namespace gol_util
 {
-    template <std::size_t n_row, std::size_t n_col>
+    template <size_t n_row, size_t n_col>
     void next_state(const std::array<std::array<char, n_col + 2>, n_row + 2> &old_state, std::array<std::array<char, n_col + 2>, n_row + 2> &new_state, sdl2_util::Renderer &renderer, std::array<std::array<SDL_Rect, n_col>, n_row> &rect_array)
     {
-        for (std::size_t row = 1; row < n_row + 1; row++)
+        for (size_t row = 1; row < n_row + 1; row++)
         {
-            for (std::size_t col = 1; col < n_col + 1; col++)
+            for (size_t col = 1; col < n_col + 1; col++)
             {
-                if (old_state.at(row).at(col) == 1)
-                { // Cell does not survive
-                    new_state.at(row).at(col) = 0;
-                    renderer.setRenderDrawColor("black");
+                char live_neighbours = countNeighbours<n_row, n_col>(old_state, row, col);
+                if (old_state.at(row).at(col) == 1 && (live_neighbours == 2 || live_neighbours == 3))
+                { // Cell does survives, rerendered as it is cleared after each frame
+                    new_state.at(row).at(col) = 1;
+                    renderer.setRenderDrawColor("white");
                 }
-                else if (old_state.at(row).at(col) == 0)
+                else if (old_state.at(row).at(col) == 0 && (live_neighbours == 3))
                 { // Cell is revived
                     new_state.at(row).at(col) = 1;
                     renderer.setRenderDrawColor("white");
                 }
-                std::cout<< col << std::endl;
+                else
+                {
+                    new_state.at(row).at(col) = 0;
+                    renderer.setRenderDrawColor("black");
+                }
+                std::cout << col << std::endl;
                 renderer.renderFillRect(&rect_array.at(row - 1).at(col - 1)); // Fill rectangle with white color
             }
         }
+    }
+    template <size_t n_row, size_t n_col>
+    char countNeighbours(const std::array<std::array<char, n_col + 2>, n_row + 2> &state, size_t col, size_t row)
+    {
+        char live_neighbours = state.at(row - 1).at(col - 1) + state.at(row).at(col - 1) + state.at(row + 1).at(col - 1) + state.at(row + 1).at(col) + state.at(row + 1).at(col + 1) + state.at(row).at(col + 1) + state.at(row - 1).at(col + 1) + state.at(row - 1).at(col);
+        return live_neighbours;
     }
 }
 
